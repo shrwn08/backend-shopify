a236d92c7b98e0ac1fe57012ac227da4e96f0cbf
Shrawan <shrwnkumar08@gmail.com>
2025-10-05 22:39:06 +0530

    routes define on the auth

--- src/server.js
+++ src/server.js
@@ -1,15 +1,34 @@
 import express from 'express';
 import connectDB from './DB/database.js';
-const app = express();
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT =process.env.PORT || 8080;
 
+const app = express();
+const PORT =process.env.PORT || 8080;
+app.use(express.json());
 
 connectDB();
 
+//localhost:8080/api/auth/signup
+//localhost:8080/api/auth/signin
+//localhost:8080/api/products
+//localhost:8080/api/cart
+
+// app.use('/products', )
+// app.use('/cart', )
+// app.use('/api/auth')
+
+
+// Error handlers
+app.use(notFound);
+app.use(errorHandler);
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ message: err.message || 'Server Error' });
+});
 
 
 app.listen(PORT, ()=>console.log (`server is running on port ${PORT}`));
