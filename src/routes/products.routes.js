2cfa6d452666e8e86d21aecbb96859c9f7f2303c
Shrawan <shrwnkumar08@gmail.com>
2025-10-05 23:21:32 +0530

    products routes completed

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import {getAllProducts, getProductById} from "../controllers/products.controllers.js";
 
 const router = express.Router();
 
-router.get("/", (req,res)=>{});
-
-router.get("/:id", (req,res)=>{});
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
 
 export default router;
