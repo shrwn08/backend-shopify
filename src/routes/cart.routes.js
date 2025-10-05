f9e611804bc35dc86670c84a3bfad26d6d2e81c3
Shrawan <shrwnkumar08@gmail.com>
2025-10-05 23:32:29 +0530

    verify middleware added in the routes

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {addToCart, updateCartItem, removeFromCart} from "../controllers/cart.controllers.js";
-
+import verifyToken from "../middlewares/verify.middlewares.js";
 
 const router = express.Router();
 
-router.post('/', addToCart);
+router.post('/', verifyToken,addToCart);
 
-router.put("/:id", updateCartItem);
+router.put("/:id",verifyToken ,updateCartItem);
 
-router.delete("/:id", removeFromCart);
+router.delete("/:id", verifyToken,removeFromCart);
 
 export default router;
