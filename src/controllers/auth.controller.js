8801e6524d850dd3078ed15fec2339fc17d86f46
Shrawan <shrwnkumar08@gmail.com>
2025-10-05 23:00:54 +0530

    loginUser added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -41,4 +41,49 @@ export const registerUser = async (req, res, next) => {
     }
 };
 
+// Login and issue a JWT token
+export const loginUser = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // Validation checks
+        const errors = {};
+
+        // If both email and username not present
+        if(!email && !username){
+            errors.identifier = "Email or username is required"
+        }
+
+        // If both username and email is present
+        if (email && username) {
+            errors.identifier = "Only one of email or username is allowed";
+        }
+
+        // If password is not present
+        if (!password) {
+            errors.password = "Password is required";
+        }
+
+        if (Object.keys(errors).length > 0) {
+            return res.status(400).json({ errors });
+        }
+
+        // Find user by either email or username and password
+        const query = email ? { email, password } : { username, password };
+        const user = await User.findOne(query);
+        if (!user) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        // Create JWT with user ID
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "1d"
+        });
+
+        res.status(200).json({ message: "Login successful", token });
+    } catch (err) {
+        next(err);
+    }
+};
+
 
