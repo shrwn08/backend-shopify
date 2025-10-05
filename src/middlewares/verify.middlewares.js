b3ec47d60cbf4e8ba6c39e6efc811eb10fbc2789
Shrawan <shrwnkumar08@gmail.com>
2025-10-05 23:26:00 +0530

    verify middleware completed

---
+++ src/middlewares/verify.middlewares.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default function verifyToken(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader) return res.status(403).json({ message: 'Unauthorized access Token missing!!!' });
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.id; // attach userId to request
+        next();
+    } catch (err) {
+        return res.status(403).json({ error: "Invalid or expired token" });
+    }
+}
